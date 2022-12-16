65aec94d66ca031d9a0d83b9a4dc028462017b14
pratikjoshi999 <pratikjoshi999@gmail.com>
2022-12-17 00:07:12 +0530

    example

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 
 
 import chatgptprtk as ch
+import os
 
-api_key="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"
-
+api_key=os.getenv('API_KEY')
 # msg="I want to learn Python, MVC, MongoDb and Azure what we will be completed soon"
 
 ch.setApiKey(api_key)
