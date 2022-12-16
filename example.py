3c4308c5b95de9ab9a503ba9a811c274831c1664
pratikjoshi999 <pratikjoshi999@gmail.com>
2022-12-17 00:01:28 +0530

    example

---
+++ example.py
@@ -0,0 +1,15 @@
+
+
+import chatgptprtk as ch
+
+api_key="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"
+
+# msg="I want to learn Python, MVC, MongoDb and Azure what we will be completed soon"
+
+ch.setApiKey(api_key)
+# print(ch.sendMessage(msg))
+
+ch.startServer()
+
+
+
